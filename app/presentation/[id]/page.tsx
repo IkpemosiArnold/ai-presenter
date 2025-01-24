b6bd5f8348654d645fe792c9c1ce79eb05ece4ad
IkpemosiArnold <Ikpemosi@protonmail.com>
2025-01-24 15:20:39 +0100

    align with page props

--- app/presentation/[id]/page.tsx
+++ app/presentation/[id]/page.tsx
@@ -1,10 +1,9 @@
 import PresentationViewer from "../../../components/PresentationViewer";
 
-export default function PresentationPage({
-  params,
-}: {
-  params: { id: string };
+export default async function PresentationPage(props: {
+  params: Promise<{ id: string }>;
 }) {
+  const params = await props.params;
   return (
     <div className="container mx-auto p-4">
       <PresentationViewer id={params.id} />

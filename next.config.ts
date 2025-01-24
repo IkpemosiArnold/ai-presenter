9e425872b76d60f811b385cc38016a1400d3baf3
IkpemosiArnold <Ikpemosi@protonmail.com>
2025-01-24 14:24:56 +0100

    minor edits

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  webpack: (config, options) => {
+    // Important: return the modified config
+    config.module.rules.push({
+      test: /\.node/,
+      use: "raw-loader",
+    });
+    return config;
+  },
 };
-
-export default nextConfig;

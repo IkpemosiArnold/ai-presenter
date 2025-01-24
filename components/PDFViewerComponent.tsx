5b283e0b8443e7d730679f13baf8bfc42fc34ed2
IkpemosiArnold <Ikpemosi@protonmail.com>
2025-01-24 20:59:31 +0100

    adjusted pdfjs versions to match

--- components/PDFViewerComponent.tsx
+++ components/PDFViewerComponent.tsx
@@ -4,7 +4,7 @@ import { Worker, Viewer } from "@react-pdf-viewer/core";
 
 export default function PDFViewer({ fileUrl }: { fileUrl: string }) {
   return (
-    <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+    <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
       <Viewer fileUrl={fileUrl} />
     </Worker>
   );

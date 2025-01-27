8c501d65bd3accdb945578fc6f3cf66dd7a5d7bf
IkpemosiArnold <61964760+IkpemosiArnold@users.noreply.github.com>
2025-01-27 11:00:31 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'TheRadar - Cyberserve',
+  title: 'Cyberserve by TheRadar',
   description: 'Cyberserve Interactive Proposal by TheRadar',
 };
 
